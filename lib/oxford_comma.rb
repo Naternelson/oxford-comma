5a0db70688158d5df11ed240b8ccedd11f5b7c0c
Nate <7nater.nelson@gmail.com>
2021-01-13 20:02:31 -0700

    commit tree b12e8c864609e7dd320513ebfe68a37a7b52b53b

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
 def oxford_comma(array)
-
+    size = array.size
+    string = ""
+    if size == 1
+        string = array.join
+    elsif size == 2
+        string = array.join(" and ")
+    else
+        string_last = array.pop
+        string_first = array.join(", ")
+        string_array = [string_first,string_last]
+        string = string_array.join(", and ")
+    end
+    string
 end
+
+# array1 = ["Cat"]
+# array2 = ["Cats","Dogs"]
+# array3 = ["Cats", "Dogs", "Birds", "Snakes"]
+
+# mega_array = [array1,array2,array3]
+
+# mega_array.each do |array|
+#     oxford_comma(array)
+# end
